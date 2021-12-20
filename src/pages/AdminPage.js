b7b3c423422909ffc7ce797b59733e290faaf2cb
unknown <junheeyeap@gmail.com>
2021-12-21 02:23:22 +0900

    making admin page #1

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -30,6 +30,7 @@ const ThirdSection = styled.section`
   position : fixed;
   width: 64px;
   border-left: 1px solid #d7dce1;
+  z-index : 1;
   @media screen and (max-width: 768px) {
     display : none;
   }
@@ -38,6 +39,8 @@ const ThirdSection = styled.section`
 const InnerDivBlock = styled.div`
   flex-direction:column;
   align-items : center;
+  height: 48px;
+  border-bottom: 1px solid #d7dce1;
 `;
 
 const NavBlock = styled.nav`
@@ -46,17 +49,55 @@ const NavBlock = styled.nav`
   left : 0;
 `;
 
+const LinkShareButton = styled.button`
+  border-radius: 4px;
+  background-color: white;
+  border: 1px solid #d7dce1;
+  text-decoration: none;
+  cursor: pointer;
+  &:hover {
+    background-color : #F5F6F8;
+  }
+`;
+
+const LinkShareBlock = styled.div`
+  
+`;
+
+const LinkSpanBlock = styled.span`
+  
+`;
+
+const SharedAnchorBlock = styled.a`
+
+`;
+
 
 const AdminPage = () => {
   return (
   <>
     <FirstSection>1</FirstSection>
     <InnerDivBlock>
-      <SecondSection>2</SecondSection>
+      <SecondSection>
+        <NavBlock>
+         앙 NAV블럭
+        </NavBlock>
+        <InnerDivBlock>
+          앙DIV 
+        </InnerDivBlock>
+        <InnerDivBlock>
+          앙DIV2 
+        </InnerDivBlock>
+      </SecondSection>
     </InnerDivBlock>
-    <InnerDivBlock>
-      <ThirdSection>3</ThirdSection>
-    </InnerDivBlock>
+    <LinkShareBlock>
+    <SharedAnchorBlock link = "https://github.com/junheeyeap">
+    </SharedAnchorBlock>
+    <LinkShareButton>Share</LinkShareButton>
+        </LinkShareBlock>
+    <ThirdSection>
+         top
+    </ThirdSection>
   </>
   );
 }
