d1ff2f1896ebd6e05ae417b334a04cdf76690c0a
unknown <junheeyeap@gmail.com>
2021-12-24 02:17:51 +0900

    making admin page #2 : cancel clone change into minimum function page

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -3,25 +3,24 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 const Wrapper = styled.div`
+  background-color: #F5F6F8;
   @media only screen and (min-width: 768px){
     max-height : 100vh;
     margin-left : 64px;
     overflow : hidden;
-  }
+  } 
 `;
 
-const LeftSideSection = styled.section`
+const AdminSection = styled.section`
   height: 100%;
   height: 100vh;
   position: fixed;
   width: 64px;
   top: 0; left: 0; bottom: 0;
-  background-color: white;
   border-right: 1px solid #d7dce1;
   z-index: 1;
   @media screen and (max-width: 768px) {
     display : none;
-    background-color: #ffffff;
     height: 48px;
     position: relative;
     z-index: 1;
@@ -29,30 +28,45 @@ const LeftSideSection = styled.section`
   }
 `;
 
-const MainSection = styled.section `
-  padding-bottom : 0!important;
-  position : relative;
-  @media screen and (min-width: 768px){
-    height:100vh;
-    display:flex;
-    flex-direction: column;
-  }
+const NavBar = styled.nav``;
+
+const NavBarInner = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  height: 64px;
+  padding: 0px 24px;
+  background-color: rgb(255, 255, 255);
+  border-bottom: 1px solid rgb(218, 222, 224);
+  transition: color 0.2s cubic-bezier(0.08, 0.59, 0.29, 0.99) 0s;
 `;
 
-const RightSideSection = styled.section`
-  @media screen and (min-width: 768px){
-  position: relative; 
-  height: 100%;
-  top : 0; right: 0; bottom:0;
-  min-height : 100vh;
-  width: 300px;
-  border-left: 1px solid #d7dce1;
+const NavBarAnchor = styled.a`
+  font-size: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  position: relative;
+  margin: 0px 40px 0px 0px;
+  &:after{
+    bottom: -22px;
+    content: "";
+    display: block;
+    height: 1.8px;
+    left: 50%;
+    position: absolute;
+    background: #d7dce1;
+    transition: width 0.2s ease 0s, left 0.2s ease 0s;
+    width: 0;
+  }
+  &:hover:after{
+    width: 100%; 
+    left: 0; 
   }
 `;
 
 const ToolBarBlock = styled.div`
-  display: block;
-  position: relative;
   background-color: #fff;
 `;
 
@@ -61,16 +75,19 @@ const ToolBarInnerBlock = styled.div`
   align-items: center;
   height: 48px;
   font-size: 14px;
-  padding: 0 16px;
+  padding: 16px;
   border-bottom: 1px solid #d7dce1;
+  @media screen and (min-width: 992px){
+    padding : 0 24px;
+    height: 64px;
+  }
 `;
-
 
 const SpanBlock = styled.span`
   flex: 0;
   white-space: nowrap;
   font-weight: 700;
-  margin-right: 4px;
+  margin-right: 20px;
 `;
 
 const ShareButton = styled.button`
@@ -79,162 +96,162 @@ const ShareButton = styled.button`
   border-radius: 4px;
   background-color: transparent;
   border: 1px solid #d7dce1;
+
   padding: 6px 12px;
   font-size: 14px;
   color: #686d73;
   text-decoration: none;
   cursor: pointer;
+  &:hover{
+    background-color: #d7dce1;
+  }
 `;
 
 const AnchorBlock = styled.a`
-  display: block;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
   text-decoration: underline;
 `;
 
-const PreviewWrapper = styled.div`
-  width: 352px;
-  height: 724px;
-  padding: 16px;
-  display: block;
-  border-radius : 4px;
-  margin: 0 auto;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform-origin: top left;
+const MakingSection = styled.section`
+  height:100%;
 `;
 
-const PhoneBlock = styled.div`
-  flex: 1 1 0%;
-  flex-direction: column;
-  -webkit-box-pack: justify;
-  justify-content: space-between;
-  padding: 24px 12px;
-  height: 100%;
-  width: 100%;
+const MakingSectionInnerBlock = styled.div`
+
 `;
 
-const MadeComponentBlock = styled.div`
-  margin: 0px auto;
-  height: 100%;
-  width: 100%;
-  max-width: 680px;
-  padding-bottom: 80px;
-`;
-
-const NavBar = styled.nav`
-  box-sizing: border-box;
-  -webkit-font-smoothing: antialiased;
-  backface-visibility: hidden !important;
-`;
-
-const NavBarInner = styled.div`
-  display: flex;
-  -webkit-box-align: stretch;
-  align-items: stretch;
-  -webkit-box-pack: start;
-  justify-content: flex-start;
-  height: 64px;
-  padding: 0px 24px;
-  background-color: rgb(255, 255, 255);
-  border-bottom: 1px solid rgb(218, 222, 224);
-  transition: color 0.2s cubic-bezier(0.08, 0.59, 0.29, 0.99) 0s;
-  @media screen and (max-width: 992px){
-    height: 48px;
-  }
-  @media screen and (max-width: 767px){
-    padding: 0px 16px;
-    -webkit-box-pack: center;
-    justify-content: center;
-  }
-`;
-
-const NavBarAnchor = styled.a`
-  font-size: 16px;
-  display: flex;
-  -webkit-box-align: center;
-  align-items: center;
-  -webkit-box-pack: center;
-  justify-content: center;
-  text-align: center;
+const ComponentBlock = styled.div`
   position: relative;
-  margin: 0px 40px 0px 0px;
-`;
-
-const AnalysticHeader = styled.header`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  cursor: pointer;
-  padding: 0px 24px;
-  height: 64px;
-  border-bottom: 1px solid rgb(215, 220, 225);
-  font-size: 14px;
-  font-weight: bold;
-`;
-
-const LinkEditingSection = styled.section`
+  border-radius: 4px;
+  background-color: white;
+  box-shadow: rgb(215 220 225) 0px 2px 0px 0px;
   width: 100%;
+  padding-bottom : 16px;
 `;
 
-const LinkEditingBlock = styled.div`
-  padding: 24px 0px;
-  min-height: 448px;
-  @media only screen and (min-width: 767px){
-  padding: 0 24px;
-  height: 64px;
-}
-  @media only screen and (min-width: 576px){
-  padding: 24px 0px;
-}
+const MakingNewComponentButton = styled.button`
+  border: none;
+  font-family: inherit;
+  align-items : center; 
+  text-align: center;
+  cursor: pointer;
+  display: block;
+  background: rgb(124, 65, 255);
+  border-radius: 12px;
+  height: 48px;
+  font-weight: 600;
+  font-size: 16px;
+  width: 70%;
+  appearance: none;
+  box-sizing: border-box;
+  vertical-align: middle;
+  &:hover{
+    background : #A071FF;
+  }
+`;
+const ComponentInnerBlock = styled.div`
+  flex: 1 1 0%;
+  padding: 16px;
+  height: 100%;
+  min-height: 448px
+  position: relative;
+  border : 1px solid #d7dce1;
 `;
 
-const AdminPage = () => {
+const ComponentForm = styled.form``;
+const ComponentFormInnerBlock = styled.div`
+  flex: 1 1 0%;
+  width: 100%;
+  padding-right: 12px;
+`;
+
+const TitleBlock = styled.div`
+  padding-top: 8px;
+  justify-content: space-between;
+  width: 100%;
+  height: 100%;
+`;
+
+const URLBlock = styled.div`
+  padding-top: 8px;
+  justify-content: space-between;
+  width: 100%;
+  height: 100%;
+`;
+
+const DeleteBlock = styled.div`
+  padding-top: 8px;
+  justify-content: right;
+  width: 100%;
+  height: 100%;
+`;
+
+const AdminPage = () =>{
   return (
-  <>
-  <Wrapper>
-    <LeftSideSection>왼쪽헤더</LeftSideSection>
-    <RightSideSection>
-      <ToolBarBlock>
-        <ToolBarInnerBlock>
-          <SpanBlock>My Linktree</SpanBlock>
-          <AnchorBlock>https://junheeyeap.github.com</AnchorBlock>
-          <ShareButton>Share</ShareButton>
-        </ToolBarInnerBlock>
-      </ToolBarBlock>
-      <PreviewWrapper>
-        <PhoneBlock>
-          <MadeComponentBlock>
-            폰 내용
-          </MadeComponentBlock>
-        </PhoneBlock>
-      </PreviewWrapper>
-    </RightSideSection>
-    <NavBar>
-      <NavBarInner>
-        <NavBarAnchor><Link to ="/admin/">Links</Link></NavBarAnchor>
-        <NavBarAnchor><Link to ="/admin/apperance">Appearnce</Link></NavBarAnchor>
-        <NavBarAnchor><Link to ="/admin/settings">Settings</Link></NavBarAnchor>
-        <NavBarAnchor><Link to ="/admin/analytics">Analytics</Link></NavBarAnchor>
-        <NavBarAnchor><Link to ="/admin/apperance">PRO</Link></NavBarAnchor>
-      </NavBarInner>
-    </NavBar>
-    <MainSection>
-      <AnalysticHeader>
-        평가 헤더 내용
-      </AnalysticHeader>
-      <LinkEditingSection>
-        <LinkEditingBlock>
-
-        </LinkEditingBlock>
-      </LinkEditingSection>
-    </MainSection>
-  </Wrapper>
-  </>
+    <>
+      <Wrapper>
+        <AdminSection>
+          앙 어드민띠
+        </AdminSection>
+        <ToolBarBlock>
+          <ToolBarInnerBlock>
+            <SpanBlock>My Linktree: </SpanBlock>
+            <AnchorBlock>https://junheeyeap.github.com</AnchorBlock>
+            <ShareButton>Share</ShareButton>
+          </ToolBarInnerBlock>
+        </ToolBarBlock>
+        <NavBar>
+          <NavBarInner>
+            <NavBarAnchor><Link to ="/admin/">Links</Link></NavBarAnchor>
+            <NavBarAnchor><Link to ="/admin/apperance">Appearnce</Link></NavBarAnchor>
+            <NavBarAnchor><Link to ="/admin/settings">Settings</Link></NavBarAnchor>
+            <NavBarAnchor><Link to ="/admin/analytics">Analytics</Link></NavBarAnchor>
+            <NavBarAnchor><Link to ="/admin/apperance">PRO</Link></NavBarAnchor>
+          </NavBarInner>
+        </NavBar>
+        <MakingSection>
+          <MakingSectionInnerBlock>
+            <MakingNewComponentButton>
+              Add new Link
+            </MakingNewComponentButton>
+            <ComponentBlock>
+              <ComponentInnerBlock>
+                <ComponentForm>
+                  <ComponentFormInnerBlock>
+                    <TitleBlock>
+                      가천대
+                    </TitleBlock>
+                    <URLBlock>
+                      http://gachon.ac.kr
+                    </URLBlock>
+                  </ComponentFormInnerBlock>
+                    <DeleteBlock>
+                      X
+                    </DeleteBlock>
+                </ComponentForm>
+              </ComponentInnerBlock>
+            </ComponentBlock>
+            <ComponentBlock>
+              <ComponentInnerBlock>
+                <ComponentForm>
+                  <ComponentFormInnerBlock>
+                    <TitleBlock>
+                      국힙원탑
+                    </TitleBlock>
+                    <URLBlock>
+                      http://iu
+                    </URLBlock>
+                    </ComponentFormInnerBlock>
+                    <DeleteBlock>
+                      X
+                    </DeleteBlock>
+                </ComponentForm>
+              </ComponentInnerBlock>
+            </ComponentBlock>
+          </MakingSectionInnerBlock>
+        </MakingSection>
+      </Wrapper>
+    </>
   );
-}
+};
 
 export default AdminPage;
