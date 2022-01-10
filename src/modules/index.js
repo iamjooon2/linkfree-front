d1ac772d4fe1aa54ec4c614ac89ed495826a561b
unknown <junheeyeap@gmail.com>
2022-01-10 21:08:52 +0900

    merge modal component with Adminpage

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,7 +2,7 @@ import {combineReducers} from 'redux';
 import linkcomponents from './linkcomponents';
 
 const rootReducer = combineReducers({
-  linkcomponents
+  linkcomponents,
 });
 
-export default rootReducer;
+export default rootReducer; 
--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,8 +1,9 @@
-import React ,{useState} from 'react';
+import React ,{useRef, useEffect, useCallback, useState } from 'react';
+import { useSpring, animated } from 'react-spring';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import Modal from '../components/UI/Modal';
 import LinkComponentsContainer from '../containers/LinkComponentContainer';
+import { MdClose } from 'react-icons/md';
 
 const Wrapper = styled.div`
   background-color: #F5F6F8;
@@ -20,7 +21,6 @@ const AdminSection = styled.section`
   width: 64px;
   top: 0; left: 0; bottom: 0;
   border-right: 1px solid #d7dce1;
-  z-index: 1;
   @media screen and (max-width: 768px) {
     display : none;
     height: 48px;
@@ -77,7 +77,7 @@ const ToolBarInnerBlock = styled.div`
   font-size: 14px;
   padding: 16px;
   border-bottom: 1px solid #d7dce1;
-  @media screen and (min-width: 992px){
+  @media screen and (min-width: 768px){
     padding : 0 24px;
     height: 64px;
   }
@@ -139,19 +139,137 @@ const MakingNewComponentButton = styled.button`
   }
 `;
 
+const Background = styled.div`
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalWrapper = styled.div`
+  width: 800px;
+  height: 500px;
+  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+  background: #fff;
+  color: #000;
+  position: relative;
+  z-index: 10;
+  border-radius: 10px;
+  display : flex;
+  align-items: center;
+  justify-content: center;
+  @media screen and (max-width: 60em) {
+  height : 100vh;
+  width: 100vw;
+  }
+`;
+
+const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  line-height: 1.8;
+  color: #141414;
+`;
+
+const ModalButton = styled.button`
+  padding: 10px 24px;
+  background: #141414;
+  color: #fff;
+  border: none;
+`;
+
+const CloseModalButton = styled(MdClose)`
+  cursor: pointer;
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  width: 32px;
+  height: 32px;
+  padding: 0;
+  z-index: 10;
+`;
+
+const InputBlock = styled.input`
+  padding : 10px;
+  justify-content: center;
+  align-items : center;
+  border-radius : 7px;
+  width : 98%;
+  border : none;
+  box-shadow : 0px 0px 3px gray;
+  margin-bottom : 10px;
+`;
+
+
 const AdminPage = () =>{
 
+  const modalRef = useRef();
   const [showModal, setShowModal] = useState(false);
   
+  const animation = useSpring({
+    config: {
+      duration: 250
+    },
+    opacity: showModal ? 1 : 0,
+    transform: showModal ? `translateY(0%)` : `translateY(-100%)`
+  });
+
   const openModal = () => {
     setShowModal(prev => !prev);
-  }
+  };
+
+  const closeModal = e => {
+    if (modalRef.current === e.target) {
+      setShowModal(false);
+    }
+  };
+
+  const keyPress = useCallback(
+    e => {
+      if (e.key === 'Escape' && showModal) {
+        setShowModal(false);
+        console.log('I pressed');
+      }
+    },
+    [setShowModal, showModal]
+  );
+
+  useEffect(
+    () => {
+      document.addEventListener('keydown', keyPress);
+      return () => document.removeEventListener('keydown', keyPress);
+    },
+    [keyPress]
+  );
 
   return (
     <>
-      <Modal showModal = {showModal} setShowModal = {setShowModal}> 
-      </Modal>
-      <Wrapper>
+      {showModal ? (
+          <Background onClick={closeModal} ref={modalRef}>
+            <animated.div style={animation}>
+              <ModalWrapper showModal={showModal}>
+                <ModalContent>
+                <form>
+                  <InputBlock placeholder = "id"/>
+                  <InputBlock placeholder = "title"/>
+                  <InputBlock placeholder = "link"/>
+                <ModalButton onClick>Post</ModalButton>
+                </form>
+                </ModalContent>
+                <CloseModalButton
+                  aria-label='Close modal'
+                  onClick={() => setShowModal(prev => !prev)}
+                />
+              </ModalWrapper>
+            </animated.div>
+          </Background>
+      ) : null}
+      <Wrapper> 
         <AdminSection>
         </AdminSection>
         <ToolBarBlock>
